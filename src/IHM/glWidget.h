4f5c32150a1d8aa2c3704da65d47210661522cec
mathias <mathias@SIC08001-64>
2013-05-27 18:14:17 +0200

    ajout des fichiers manquants (fichiers nécessaires qui n'étaient pas sous versionnement)

---
+++ src/IHM/glWidget.h
@@ -0,0 +1,101 @@
+#ifndef GLWIDGET_H
+#define GLWIDGET_H
+
+
+//************************************************************************/
+//************************************************************************/
+
+#include <QWidget>
+#include <QtGui>
+#include <QtOpenGL/QtOpenGL>
+#include <math.h>
+#include <QGLViewer/qglviewer.h>
+#include <QGLViewer/vec.h>
+#include <vector>
+
+#include <AnimatedHeightField.h>
+#include <System.h>
+#include <SimpleSystem.h>
+#include <CudaSystem.h>
+#include <SphSystem.h>
+#include <PciSphSystem.h>
+#include <SimulationData_CudaSystem.h>
+#include <SimulationData_SimpleSystem.h>
+#include <SimulationData_SPHSystem.h>
+#include <SimulationData_PCI_SPHSystem.h>
+#include <glDisplay.h>
+#include <ObjectCollision.h>
+#include <SurfaceSPH.h>
+
+//************************************************************************/
+//************************************************************************/
+
+using namespace qglviewer;
+using namespace std;
+
+//************************************************************************/
+//************************************************************************/
+ class GLWidget : public QGLViewer
+ {
+     Q_OBJECT
+
+ public:
+
+   //******************************************************************************
+   // GETTERS ET SETTERS
+   //******************************************************************************
+
+     System*  getSystem();
+     void     setSystem(System*);
+
+     GLDisplay* getDisplay();
+     void       setDisplay(GLDisplay*);
+
+     SurfaceSPH* getSurface();
+     
+     AnimatedHeightField* getAnimatedHF();
+     void setAnimatedHF(AnimatedHeightField * hf);
+
+  //******************************************************************************
+  // SURCHARGE DES FONCTIONS
+  //******************************************************************************
+     virtual void  init();
+     virtual void  draw();
+     virtual void  animate();
+     virtual QSize minimumSizeHint() const;
+     virtual QSize sizeHint() const;
+
+  //******************************************************************************
+  // FONCTIONS PROPRES
+  //******************************************************************************
+     void removeAll();
+     void initSystem();
+
+     void captureVideo(bool);
+     void captureImagesSequence();
+
+     void displaySurface();
+
+  private :
+
+     //******************************************************************************
+     System *system;
+ 
+     //******************************************************************************
+     GLDisplay *display;
+
+     //******************************************************************************
+     SurfaceSPH *surface;
+     
+     AnimatedHeightField *animatedHF;
+
+     //******************************************************************************
+     unsigned int frame_courante;
+     bool captureImage;
+
+     //******************************************************************************
+     void initRender();
+ 
+ };
+
+#endif
